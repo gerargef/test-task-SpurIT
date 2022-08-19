107028ebc26732ebd550c74e996c7f03ccd7d077
gerargef <111460557+gerargef@users.noreply.github.com>
2022-08-19 23:24:17 +0300

    Коллапс

--- тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
+++ тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
@@ -1,1 +1,25 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const collapsibleButton = document.querySelector ('.collapsible__button');
+const collapsiblContent = document.querySelector('.collapsible__content');
+
+const toHide = collapsiblContent.animate({
+    opacity:[1,0],
+    }, 3000);
+
+const toShow = collapsiblContent.animate({
+    opacity:[0,1],
+    }, 3000);   
+
+collapsiblContent.style.display = "block"
+
+collapsibleButton.onclick = function () {
+    if (collapsiblContent.style.display === "block") {
+        toHide.play();
+        setTimeout(function() {
+            collapsiblContent.style.display = "none";
+        }, 3000);
+            
+      } else {
+            collapsiblContent.style.display = "block";
+            toShow.play();
+      }
+    }
